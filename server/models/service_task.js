5daa642ec127c9b5160340d2e10e44527695290f
alvinfnaldi <alvinfnaldi@gmail.com>
2023-12-03 17:19:19 +0700

    add association

--- server/models/service_task.js
+++ server/models/service_task.js
@@ -10,8 +10,8 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Service_Task.hasMany(models.work_order_details, {
-        foreignKey: "wode_seta_id"
-      })
+        foreignKey: "wode_seta_id",
+      });
     }
   }
   Service_Task.init(
