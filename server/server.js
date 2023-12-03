6a8c270e64ab4bcac095d6e909bd844adf8c5ea5
Helasdic <helasdicakra@gmail.com>
2023-12-04 01:24:48 +0700

    update server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const routes = require("./route/master/route.js");
 const routes_HR = require("./route/HR");
 const express = require("express");
 const cors = require("cors");
-const routes = require("././route/Payment/index");
+const routesPayment = require("././route/Payment/index");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(routes);
 app.use(routes_HR);
+app.use(routesPayment);
 app.use(express.static("public"));
 
 app.listen(port, () => {
