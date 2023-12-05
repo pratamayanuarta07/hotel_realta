486bcf39c401122c2087a62cc319e3d61e57d2b2
Wahyu Said <37928500+wahyuside@users.noreply.github.com>
2023-12-06 06:48:28 +0700

    Delete old server directory

--- server/route/index.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const purchasingRoute = require("./purchasing");
-
-const route = Router();
-
-route.use("/purchasing", purchasingRoute);
-
-module.exports = route;
